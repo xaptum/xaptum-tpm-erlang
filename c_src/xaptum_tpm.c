0d9ab0c1d66a1e52465e46bc1aa99fe0d47baaef
Irina Guberman <irina.guberman@gmail.com>
2018-04-06 18:43:32 -0500

    fix init_socket tcti context allocation

--- c_src/xaptum_tpm.c
+++ c_src/xaptum_tpm.c
@@ -68,7 +68,7 @@ tss2_tcti_initialize_socket(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
     TSS2_RC rc =
     tss2_tcti_init_socket(hostname.data,
                           port.data,
-                          (TSS2_TCTI_CONTEXT *) tcti_context_buffer_bin.data);
+                          (TSS2_TCTI_CONTEXT *) tcti_context_bin.data);
 
     if (rc == TSS2_RC_SUCCESS) {
         ERL_NIF_TERM tcti_resource = enif_make_resource_binary(env, tcti_context, &(tcti_context_bin.data), tcti_context_bin.size);
