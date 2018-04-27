c14c91b8cbf8686f3caa66d3794db148c9fd52c8
Irina Guberman <irina.guberman@gmail.com>
2018-04-27 14:52:58 -0500

    ensure only one sapi/tcti combo per TPM server during program execution

--- src/xaptum_tpm.erl
+++ src/xaptum_tpm.erl
@@ -73,12 +73,12 @@ part2_error_code(_Unclassified)->"Unclassified!".
 
 init() ->
   application:ensure_all_started(lager),
+  tpm = ets:new(tpm, [named_table, set, public, {read_concurrency, true}]),
   SoName = filename:join([priv_dir(), ?TPM_LIBNAME]),
   lager:info("Loading NIFs from ~p", [SoName]),
   case erlang:load_nif(SoName, 0) of
     ok ->
-      lager:info("Successfully loaded NIFs from ~p", [SoName]),
-      tpm = ets:new(tpm, [named_table, set, public, {read_concurrency, true}]);
+      lager:info("Successfully loaded NIFs from ~p", [SoName]);
     {error, {reload, ReloadMessage}} ->
       lager:info("Reload attempt: ~p", [ReloadMessage]),
       ok;
